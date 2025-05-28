4edd9616ff0cb61f40f491ea1a539169fd2fd782
JustineDwayne <163003030+JustineDwayne@users.noreply.github.com>
2025-05-29 00:25:02 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ function App() {
     <div className="flex flex-col min-h-screen scroll-smooth" section id="home">
       <Header />
       <Main />
+      
       <section id="about" className="scroll-mt-[5rem]">
         <About />
       </section>
+
       <section id="stacks" className="scroll-mt-[5rem]">
       <Icons />
       </section>
