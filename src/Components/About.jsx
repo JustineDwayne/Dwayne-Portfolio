4489715e33be5f3b4a273f867bf0e6cef791c09d
JustineDwayne <163003030+JustineDwayne@users.noreply.github.com>
2025-05-29 00:29:50 +0800

    Changed ico

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,7 +5,7 @@ import { FaLinkedin, FaGithub } from "react-icons/fa";
 export default function About() {
     return (
         <section
-            className="relative flex-1 flex flex-col md:flex-row bg-gradient-to-b from-black to-zinc-950 md:p-10 bg-cover bg-center"
+            className="relative flex-1 flex flex-col md:flex-row bg-gradient-to-b bg-black from-black to-zinc-950 md:p-10 bg-cover bg-center"
         >
 
             <div className="flex flex-col justify-center max-w-4xl pt-8 px-10 md:max-w-none md:flex-1">
